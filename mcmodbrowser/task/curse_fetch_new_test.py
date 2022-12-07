902b845198711d70131bd69bb3bcd5c617e46812
makamys <makamys@outlook.com>
2022-12-07 21:34:13 +0100

    Add search test

---
+++ mcmodbrowser/task/curse_fetch_new_test.py
@@ -0,0 +1,23 @@
+import requests
+import datetime
+from mcmodbrowser.util import loadJson, writeJson, getCurseToken
+from mcmodbrowser.model.curse import getCurseHeaders
+from mcmodbrowser.index import writeCurseModToIndex, getCurseModLastModifiedTimestamp
+
+def run():
+    '''Fetch recently updated mods and put them in the index.'''
+    
+    curseToken = getCurseToken()
+
+    for searchIndex in range(0, 10000, 50):
+        print(searchIndex, "/", 9950)
+        resp = requests.get("https://api.curseforge.com/v1/mods/search?gameId=432&sortField=3&sortOrder=desc&pageSize=50&index={}".format(searchIndex), headers = getCurseHeaders(curseToken))
+        
+        if resp.status_code == 200:
+            assert "data" in resp.json()
+            
+            writeJson(resp.json(), "data/search_test/{}.json".format(searchIndex))
+        else:
+            print("ERROR: got status code", resp.status_code)
+            break
+    
