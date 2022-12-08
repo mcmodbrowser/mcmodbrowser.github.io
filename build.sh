43303dbfd00c2c3d8fefd00cc4331dd0d6783761
makamys <makamys@outlook.com>
2022-12-08 05:19:02 +0100

    Actually readd python

--- build.sh
+++ build.sh
@@ -2,14 +2,8 @@ echo "creating public dir"
 
 mkdir -p public
 
-echo "echoing hi"
+echo "running hello.py"
 
-echo "<h1>hi</h1>" > public/index.html
+python3 hello.py
 
-echo "end of build.sh"
-
-#echo "running hello.py"
-
-#python3 hello.py
-
-#echo "ran hello.py"
+echo "ran hello.py"
