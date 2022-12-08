aa10ca6ed0b23f48397b747bde6833b1018ae34e
makamys <makamys@outlook.com>
2022-12-08 05:28:35 +0100

    add persistence test

--- hello.py
+++ hello.py
@@ -1,7 +1,26 @@
-print("hello internet, pythin is running now.")
+import os
+import datetime
+
+memory = []
+
+if not os.path.isdir("data"):
+    os.mkdir("data")
+
+if os.path.exists("data/memory.txt"):
+    memory = [x.strip() for x in list(open("data/memory.txt", "r", encoding="utf8"))]
+
+print("memory:", memory)
+
+memory.append("It is now " + datetime.datetime.now(datetime.timezone.utc).isoformat())
 
 with open("public/index.html", "w", encoding="utf8") as fp:
     fp.write("<p>hi.</p>")
+    
+    for entry in memory:
+        fp.write("<p>" + entry + "</p>")
 
 with open("public/text.txt", "w", encoding="utf8") as fp:
     fp.write("this is text")
+
+with open("data/memory.txt", "w", encoding="utf8") as fp:
+    fp.write("\n".join(memory))
