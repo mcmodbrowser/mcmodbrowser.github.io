17dc0ea71b1049a4eee5571e43484257cad57625
makamys <makamys@outlook.com>
2022-12-08 05:16:50 +0100

    Re-add python
    * Revert "Revert "Test Python""
    
      This reverts commit bf974aa7ac36a3b9666da4f48d5e0cdb6497365c.
    
    * Revert "Revert "Why it no work""
    
      This reverts commit 8d382ff6e6065cdefa30194ebf9c90f2310d3400.

---
+++ hello.py
@@ -0,0 +1,7 @@
+print("hello internet, pythin is running now.")
+
+with open("public/index.html", "w", encoding="utf8") as fp:
+    fp.write("<p>hi.</p>")
+
+with open("public/text.txt", "w", encoding="utf8") as fp:
+    fp.write("this is text")
